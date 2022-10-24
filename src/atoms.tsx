5bbfd20b436f54b78c4e63980b4e62bf686f0c63
Pildrums <there7788@gmail.com>
2022-10-24 17:52:01 +0900

    Commented atoms.tsx

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,10 +1,16 @@
 import { atom, selector } from "recoil";
 
+/**
+ * @constant minuteState - minutes 초기 상태값
+ */
 export const minuteState = atom({
   key: "minutes",
   default: 0,
 });
 
+/**
+ * @constant hourSelector - minute에서 60을 나눠서 hours input에 표현하는 selector
+ */
 export const hourSelector = selector({
   key: "hours",
   get: ({ get }) => {
