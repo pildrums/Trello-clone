1a3753c70a4364082b8f0855b011a0ac324ebe57
Pildrums <there7788@gmail.com>
2022-10-24 18:16:17 +0900

    Added hourSelector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -10,11 +10,17 @@ export const minuteState = atom({
 
 /**
  * @constant hourSelector - minute에서 60을 나눠서 hours input에 표현하는 selector
+ * @description get은 atom에 접근할 때 사용
+ * @description set은 atom의 초기값을 수정할 때 사용
  */
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key: "hours",
   get: ({ get }) => {
     const minutes = get(minuteState);
     return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
