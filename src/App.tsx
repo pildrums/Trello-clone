6b0524de3bd60c3935c1744a8f086cf9cc70d01e
Pildrums <there7788@gmail.com>
2022-10-24 19:15:09 +0900

    Set up react-beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function App() {
-  return null;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">{() => <ul>
+          <Draggable draggableId="first" index={0}>
+            {() => <li>One</li>}
+          </Draggable>
+          <Draggable draggableId="second" index={1}>
+            {() => <li>Two</li>}
+          </Draggable>
+          </ul>}</Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
