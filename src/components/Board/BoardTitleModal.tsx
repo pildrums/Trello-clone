67967aa5d3f462cb3dfef711d73bf0c1317ee6af
Pildrums <there7788@gmail.com>
2022-10-31 15:23:55 +0900

    Completed BoardTitleModal UI

--- src/components/Board/BoardTitleModal.tsx
+++ src/components/Board/BoardTitleModal.tsx
@@ -1,4 +1,9 @@
-import { boardTitleModalState, ITodoState, todoState } from "atoms";
+import {
+  boardTitleModalState,
+  boardTitleState,
+  ITodoState,
+  todoState,
+} from "atoms";
 import ModalContainer from "components/common/ModalContainer";
 import { useForm } from "react-hook-form";
 import { SetterOrUpdater, useRecoilState, useSetRecoilState } from "recoil";
@@ -8,23 +13,37 @@ interface IFormData {
 }
 
 function BoardTitleModal() {
+  // state
   const [boardTitleModal, setBoardTitleModal] =
     useRecoilState<boolean>(boardTitleModalState);
   const { register, handleSubmit, getValues, setValue } = useForm<IFormData>({
     mode: "onChange",
   });
   const setTodos: SetterOrUpdater<ITodoState> = useSetRecoilState(todoState);
-  
+  const [boardTitle, setBoardTitle] = useRecoilState<string>(boardTitleState);
+
+  // function
   const handleCloseModal = () => {};
+  const onValid = () => {};
+
   return (
     <ModalContainer
-      isOpen={boardTitleModalState}
+      isOpen={boardTitleModal}
       onRequestClose={handleCloseModal}
       ariaHideApp={false}
       contentLabel="boardTitleModal"
     >
       <button onClick={handleCloseModal}>X</button>
-      <form></form>
+      <form onSubmit={handleSubmit(onValid)}>
+        <div>
+          <h1>보드 수정</h1>
+          <input
+            {...register("title", { required: "보드를 수정하세요." })}
+            type="text"
+            placeholder="보드를 수정하세요."
+          />
+        </div>
+      </form>
     </ModalContainer>
   );
 }
